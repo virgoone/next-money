a777a31911c7b9869b98700587dca6a3ed2f9971
virgoone <w2008second@gmail.com>
2024-10-11 11:41:49 +0800

    fix: log hashids

--- lib/hashid.ts
+++ lib/hashid.ts
@@ -3,5 +3,7 @@ import BaseHashIds from "hashids";
 import { env } from "@/env.mjs";
 
 export const Hashids = (prefix: string, length = 12) => {
-  return new BaseHashIds(`${env.HASHID_SALT}:${prefix}`, length);
+  const salt = `${env.HASHID_SALT}:${prefix}`
+  console.log('salt-->', salt)
+  return new BaseHashIds(salt, length);
 };
